6c17b89029dee4ce557d157090b841d618eca622
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-09-14 17:43:05 -0500

    test

--- candidate-testing.js
+++ candidate-testing.js
@@ -55,6 +55,7 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+runProgram();
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
